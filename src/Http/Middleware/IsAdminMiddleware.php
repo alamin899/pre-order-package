dbedeaa85a65a297a66152a7a48f4eefaee88a82
alaminAppnap <alamin@appnap.io>
2024-11-28 13:00:31 +0600

    [PO-5] feat: Role base authorization by middleware

---
+++ src/Http/Middleware/IsAdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PreOrder\PreOrderBackend\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use PreOrder\PreOrderBackend\Facade\CustomAuth;
+
+class IsAdminMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!CustomAuth::check()) {
+            return redirect()->route('auth.login')->with('error', 'Please log in first.');
+        } else {
+            $user = CustomAuth::user();
+
+            if ($user && $user->roles->contains('name', 'admin')) {
+                return $next($request);
+            } else {
+                abort(403);
+            }
+        }
+    }
+}
