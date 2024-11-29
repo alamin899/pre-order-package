e6e54a9f50368e94a14bf3a661236997317b4500
alamin899 <alaminhossen899@gmail.com>
2024-11-29 08:44:20 +0600

    [master] feat: recaptch update

--- src/Http/Requests/PreOrderRequest.php
+++ src/Http/Requests/PreOrderRequest.php
@@ -23,9 +23,12 @@ class PreOrderRequest extends FormRequest
             'products' => ['required', 'array', 'min:1'],
             'products.*.slug' => ['required', 'exists:po_products,slug'],
             'products.*.quantity' => ['required', 'integer', 'min:1'],
-            'gRecaptchaToken' => ['required', 'min:5', 'max:1000', new GoogleReCaptchaRule()],
         ];
 
+        $recaptcha = config('setting.google');
+        if (!$recaptcha['bypass_captcha']) {
+            $rules['gRecaptchaToken'] = ['required', 'min:5', 'max:1000', new GoogleReCaptchaRule()];
+        }
         if ($this->filled('customer_email') && str_ends_with($this->input('customer_email'), '@xyz.com')) {
             $rules['customer_phone'] = ['required', 'max:16', 'min:11', 'regex:/^(\+88|88|0088|\+0088)?(01[3-9]\d{8})$/'];
         }
